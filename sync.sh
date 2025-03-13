25374daa0b4bdb0d53e124dd364d50000d310596
Lucas Pena <lucaspena4@gmail.com>
2025-03-13 18:42:18 -0300

    Updated via sync.sh

--- sync.sh
+++ sync.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+echo "Syncing files with GitHub..."
+
+echo -e "\npull"
 git pull
+
+echo -e "\nadd"
 git add .
+
+echo -e "\ncommit"
 git commit -m "Updated via sync.sh"
+
+echo -e "\npush"
 git push
 
 echo -e "\n\nFiles synced with GitHub."
